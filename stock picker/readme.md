b67f401c834eadc5553d08fe890c3568b022f8ba
Deepesh2023 <officialdeepeshsg@gmail.com>
2023-11-17 15:41:29 +0530

    completed stock picker

---
+++ stock picker/readme.md
@@ -0,0 +1,1 @@
+a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
---
+++ stock picker/stock_picker.rb
@@ -0,0 +1,13 @@
+def stock_picker (stocks)
+  # create an array for each stock with their best selling day profit
+  price_difference = Array.new
+  0.upto(stocks.length - 1) do |i|
+    price_difference.push(stocks[i..stocks.length - 1].max - stocks[i])
+  end
+  buying_day = price_difference.index(price_difference.max)
+  selling_day = stocks.index(best_stocks.max + stocks[buying_day])
+
+  [buying_day, selling_day]
+end
+
+puts stock_picker([17,3,6,9,15,8,6,1,10])
